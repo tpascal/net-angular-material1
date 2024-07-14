c8349520d4580b5a08d78613202a123bfbbb03bd
Tony Pascal <tony_pascal@yahoo.com.au>
2024-07-14 12:08:22 +0930

    add basic components and routing

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
 import { NavMenuComponent } from "./nav-menu/nav-menu.component";
 import { HomeComponent } from "./home/home.component";
 import { CounterComponent } from "./counter/counter.component";
 import { FetchDataComponent } from "./fetch-data/fetch-data.component";
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +27,13 @@ import { FetchDataComponent } from "./fetch-data/fetch-data.component";
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
@@ -26,11 +41,12 @@ import { FetchDataComponent } from "./fetch-data/fetch-data.component";
     FormsModule,
     BrowserAnimationsModule,
     MaterialModule,
-    RouterModule.forRoot([
-      { path: "", component: HomeComponent, pathMatch: "full" },
-      { path: "counter", component: CounterComponent },
-      { path: "fetch-data", component: FetchDataComponent },
-    ]),
+    AppRoutingModule
+    // RouterModule.forRoot([
+    //   { path: "", component: HomeComponent, pathMatch: "full" },
+    //   { path: "counter", component: CounterComponent },
+    //   { path: "fetch-data", component: FetchDataComponent },
+    // ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
