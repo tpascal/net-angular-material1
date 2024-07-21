a429f6ecbe4cbec99dfb30f6eef4312ba5b62e97
Tony Pascal <tony_pascal@yahoo.com.au>
2024-07-21 17:15:51 +0930

    add exercise timer

--- ClientApp/src/app/training/current-training/current-training.component.ts
+++ ClientApp/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import { clearInterval } from 'timers';
 
 @Component({
   selector: 'app-current-training',
@@ -7,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
+  timer: number;
 
   constructor() { }
 
   ngOnInit() {
-    setInterval(() => {
+    this.timer = setInterval(() => {
       this.progress = this.progress + 5;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
     }, 1000);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
   }
 
 }
