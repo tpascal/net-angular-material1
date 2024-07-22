82fc978b4dacbb4a513b02db2f723fe7c4a7fae3
Tony Pascal <tony_pascal@yahoo.com.au>
2024-07-22 20:27:54 +0930

    setting an active exercise

--- ClientApp/src/app/training/training.service.ts
+++ ClientApp/src/app/training/training.service.ts
@@ -1,14 +1,19 @@
 import { Exercise } from "./exercise.model";
 
 export class TrainingService {
-    private availableExercise: Exercise[] = [
+    private availableExercises: Exercise[] = [
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
         { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
         { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
         { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
     ];
+    private runningExercise: Exercise;
 
     getAvailableExercises() {
-        return this.availableExercise.slice();
+        return this.availableExercises.slice();
+    }
+
+    startExercise(selectedId: string) {
+        this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
     }
 }
