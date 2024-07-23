b98d327535eadbae2b3ad472364b194d165d20f7
Tony Pascal <tony_pascal@yahoo.com.au>
2024-07-23 21:06:32 +0930

    handling active training via service

--- ClientApp/src/app/training/current-training/current-training.component.ts
+++ ClientApp/src/app/training/current-training/current-training.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { StopTrainingComponent } from './stop-training.component';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-current-training',
@@ -13,19 +14,21 @@ export class CurrentTrainingComponent implements OnInit {
   timer: number;
 
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private trainingService: TrainingService) { }
 
   ngOnInit() {
     this.startOrResumeTimer();
   }
 
   startOrResumeTimer() {
+    const step = this.trainingService.getRunningExercise().duration / 100 * 1000;
+    
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
-    }, 1000);
+    }, step);
   }
 
   onStop() {
