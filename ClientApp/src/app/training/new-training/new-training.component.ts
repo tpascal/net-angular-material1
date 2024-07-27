c6beef5019723a42990fe953a52839643c94489f
Tony Pascal <tony_pascal@yahoo.com.au>
2024-07-27 21:17:24 +0930

    listening to snapshot changes

--- ClientApp/src/app/training/new-training/new-training.component.ts
+++ ClientApp/src/app/training/new-training/new-training.component.ts
@@ -5,6 +5,7 @@ import { NgForm } from '@angular/forms';
 
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -12,13 +13,26 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    //this.exercises = this.trainingService.getAvailableExercises();
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map(docArray => {
+          return docArray.map(doc => {
+            return {
+              id: doc.payload.doc.id,
+              //...doc.payload.doc.data()
+              name: doc.payload.doc.data()['name'],
+              duration: doc.payload.doc.data()['duration'],
+              calories: doc.payload.doc.data()['calories']
+            };
+          });
+      }));
   }
 
   onStartTraining(form: NgForm) {
