74f15ed9271f18aafbb2cb9aae561b625e7e02a9
Tony Pascal <tony_pascal@yahoo.com.au>
2024-07-27 23:13:39 +0930

    storing completed exercises on firebase

--- ClientApp/src/app/training/training.service.ts
+++ ClientApp/src/app/training/training.service.ts
@@ -43,13 +43,13 @@ export class TrainingService {
     }
 
     completeExercise() {
-        this.exercises.push({ ...this.runningExercise, date: new Date(), state: 'completed' });
+        this.addDataToDatabase({ ...this.runningExercise, date: new Date(), state: 'completed' });
         this.runningExercise = null;
         this.exerciseChanged.next(null);
     }
 
     cancelExercise(progress: number) {
-        this.exercises.push({
+        this.addDataToDatabase({
             ...this.runningExercise,
             duration: this.runningExercise.duration * (progress / 100),
             calories: this.runningExercise.calories * (progress / 100),
@@ -65,5 +65,9 @@ export class TrainingService {
 
     getCompletedOrCancelledExercises() {
         return this.exercises.slice();
+    }
+
+    private addDataToDatabase(exercise: Exercise) {
+        this.db.collection('finishedExercises').add(exercise);
     }
 }
